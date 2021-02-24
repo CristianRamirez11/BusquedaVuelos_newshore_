fc53b249006ed1839e393bfcd8697ebc2b05bdc6
Cristian Ramirez <cristian.ramirez@laaurora.co>
2021-02-23 22:22:17 -0500

    se crean las entidades del modelo

---
+++ BusquedaVuelos/Models/Flight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusquedaVuelos.Models
+{
+    public class Flight
+    {
+        public string DepartureStation { get; set; }
+        public string ArrivalStation { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public virtual Transport Transport { get; set; }
+        public decimal Price { get; set; }
+        public string Currency { get; set; }
+    }
+}
---
+++ BusquedaVuelos/Models/Transport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusquedaVuelos.Models
+{
+    public class Transport
+    {
+        public string FlightNumber { get; set; }
+    }
+}
