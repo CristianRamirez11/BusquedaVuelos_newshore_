ae8f271f00aa85283386f38523817001d5fa41e9
Cristian Ramirez <cristian.ramirez@laaurora.co>
2021-02-24 23:46:24 -0500

    se crean los controllers

---
+++ BusquedaVuelos/Controllers/FlightController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BusquedaVuelos.Context;
+using BusquedaVuelos.Models;
+
+namespace BusquedaVuelos.Controllers
+{
+    public class FlightController : Controller
+    {
+        private AplicationDBContext db = new AplicationDBContext();
+
+        // GET: Flight
+        public ActionResult Index()
+        {
+            var flights = db.Flights.Include(f => f.Transport);
+            return View(flights.ToList());
+        }
+
+        // GET: Flight/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Flight flight = db.Flights.Find(id);
+            if (flight == null)
+            {
+                return HttpNotFound();
+            }
+            return View(flight);
+        }
+
+        // GET: Flight/Create
+        public ActionResult Create()
+        {
+            ViewBag.TransportId = new SelectList(db.Transports, "Id", "FlightNumber");
+            return View();
+        }
+
+        // POST: Flight/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,DepartureStation,ArrivalStation,DepartureDate,TransportId,Price,Currency")] Flight flight)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Flights.Add(flight);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TransportId = new SelectList(db.Transports, "Id", "FlightNumber", flight.TransportId);
+            return View(flight);
+        }
+
+        // GET: Flight/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Flight flight = db.Flights.Find(id);
+            if (flight == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TransportId = new SelectList(db.Transports, "Id", "FlightNumber", flight.TransportId);
+            return View(flight);
+        }
+
+        // POST: Flight/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,DepartureStation,ArrivalStation,DepartureDate,TransportId,Price,Currency")] Flight flight)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(flight).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TransportId = new SelectList(db.Transports, "Id", "FlightNumber", flight.TransportId);
+            return View(flight);
+        }
+
+        // GET: Flight/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Flight flight = db.Flights.Find(id);
+            if (flight == null)
+            {
+                return HttpNotFound();
+            }
+            return View(flight);
+        }
+
+        // POST: Flight/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Flight flight = db.Flights.Find(id);
+            db.Flights.Remove(flight);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BusquedaVuelos/Controllers/TransportController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BusquedaVuelos.Context;
+using BusquedaVuelos.Models;
+
+namespace BusquedaVuelos.Controllers
+{
+    public class TransportController : Controller
+    {
+        private AplicationDBContext db = new AplicationDBContext();
+
+        // GET: Transport
+        public ActionResult Index()
+        {
+            return View(db.Transports.ToList());
+        }
+
+        // GET: Transport/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transport transport = db.Transports.Find(id);
+            if (transport == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transport);
+        }
+
+        // GET: Transport/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Transport/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FlightNumber")] Transport transport)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Transports.Add(transport);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(transport);
+        }
+
+        // GET: Transport/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transport transport = db.Transports.Find(id);
+            if (transport == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transport);
+        }
+
+        // POST: Transport/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FlightNumber")] Transport transport)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(transport).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(transport);
+        }
+
+        // GET: Transport/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transport transport = db.Transports.Find(id);
+            if (transport == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transport);
+        }
+
+        // POST: Transport/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Transport transport = db.Transports.Find(id);
+            db.Transports.Remove(transport);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
