0b1b4bddc74c9ca8ed86f68027647be84f23b86f
Cristian Ramirez <cristian.ramirez@laaurora.co>
2021-02-25 23:44:36 -0500

    Servicio API

---
+++ BusquedaVuelos/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusquedaVuelos.Models
+{
+    public class Response
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string From { get; set; }
+    }
+}
---
+++ BusquedaVuelos/Utility/Resource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusquedaVuelos.Utility
+{
+    public class Resource
+    {
+        public const string APIBaseUrl = "http://localhost:62006/";
+        public const string FlightAPIUrl = APIBaseUrl + "api/flight/";
+        public const string ContentType = "application/json";
+    }
+}
